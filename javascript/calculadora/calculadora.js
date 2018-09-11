e41caff62d3e723771b0cf8dc57848a301460013
Sebastian <sebaslondono2@utp.edu.co>
2018-09-10 21:21:50 -0500

    practice js

--- javascript/calculadora/calculadora.js
+++ javascript/calculadora/calculadora.js
@@ -1,8 +1,9 @@
 function suma(){
-	var numero1 = parseFloat(document.getElementById('numero1').value);//pasefloat transforma lo que tiene adentro del parentesisen decimales
+	var numero1 = parseFloat(document.getElementById('numero1').value);//pasefloat transforma lo que tiene adentro del parentesis en decimales
 	var numero2 = parseFloat(document.getElementById('numero2').value);
 	var resultado = numero1 + numero2;
 
+	// con getelementbyid se obtiene el elemento de la pagina
 	document.getElementById('resultado').value = resultado;
 	//queremos ingresar al id con el nombre resultado y con value vamos a acceder a su valor
 }
